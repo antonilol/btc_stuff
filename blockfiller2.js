95fc2d27a37d08a510ec0292c7a4291cb21a26c2
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-28 21:15:22 +0100

    blockfiller2.js

---
+++ blockfiller2.js
@@ -0,0 +1,54 @@
+const { newtx, listunspent } = require('./btc');
+
+var count = 0;
+
+main();
+
+async function main() {
+	console.log('Listing UTXOs');
+
+	var s = (await listunspent(0.00000110, 0))
+		.filter(u => u.spendable && u.solvable);
+
+	console.log(`Found ${s.length} usable UTXO${s.length == 1 ? '' : 's'}`);
+
+	if (s.length < 0) {
+		console.log('No UTXOs!');
+		process.exit(1);
+	}
+
+	console.log('Sending TXs of 109.25 vB each');
+
+	const work = [];
+	const workers = 4;
+	const share = s.length / workers;
+
+	for (var i = 0; i < workers; i++) {
+		work.push(s.slice(i * share, workers - 1 == i ? undefined : ((i+1) * share)));
+	}
+
+	work.forEach((x, i) => {
+		loop(s, i+1);
+	});
+}
+
+async function loop(s, i) {
+	while (s.length) {
+		const u = s.shift();
+		if (u.amount < 110e-8) {
+			continue;
+		}
+		const amount = parseFloat((u.amount - 110e-8).toFixed(8));
+		const out = {};
+		const address = u.address;
+		out[address] = amount;
+		try {
+			const txid = await newtx([ u ], out);
+			s.push({ txid, vout: 0, address, amount });
+			count++;
+			console.log(`(${i}) Sent transaction txid=${txid} (#${count})`);
+		}
+		catch (e) {
+		}
+	}
+}
