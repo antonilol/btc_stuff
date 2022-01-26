c38f3cc93c7046002ab367452c94a8d548e99fd0
Antoni Spaanderman <56turtle56@gmail.com>
2022-01-26 20:49:12 +0100

    add p2wsh_relative_timelock.js

---
+++ p2wsh_relative_timelock.js
@@ -0,0 +1,60 @@
+const { btc, send, bech32toScriptPubKey } = require('./btc')();
+const bitcoin = require('bitcoinjs-lib');
+const network = bitcoin.networks.testnet;
+const hashtype = bitcoin.Transaction.SIGHASH_ALL;
+const ECPair = require('ecpair').ECPairFactory(require('tiny-secp256k1'));
+
+const txid = '1234....'; // txid hex here
+const vout = 0;
+
+// address to lock the coins
+const addr = 'tb1qbech32addresshere';
+
+// relative timelock in blocks, meaning it is spendable after n confirmations
+const tl = 5;
+
+main();
+
+async function main() {
+	const key = btc('dumpprivkey', addr);
+
+	const ecpair = ECPair.fromWIF(await key, network);
+
+	const witnessScript = bitcoin.script.compile([
+		ecpair.publicKey,
+		bitcoin.opcodes.OP_CHECKSIGVERIFY,
+		bitcoin.script.number.encode(tl),
+		bitcoin.opcodes.OP_CHECKSEQUENCEVERIFY
+	]);
+
+	console.log(
+		'send 1000 sat to ' +
+		bitcoin.payments.p2wsh({ redeem: { output: witnessScript, network }, network }).address
+	);
+
+	const tx = new bitcoin.Transaction(network);
+
+	// OP_CHECKSEQUENCEVERIFY doesn't work on version 1
+	tx.version = 2;
+
+	tx.addInput(Buffer.from(txid, 'hex').reverse(), vout, tl);
+
+	const amount = 1000;
+	const fee = 100;
+
+	tx.addOutput(bech32toScriptPubKey('tb1qbech32addresshere'), amount - fee);
+
+	const sighash = tx.hashForWitnessV0(0, witnessScript, amount, hashtype);
+	const witness = bitcoin.payments.p2wsh({
+		redeem: {
+			input: bitcoin.script.compile([
+				bitcoin.script.signature.encode(ecpair.sign(sighash), hashtype)
+			]),
+			output: witnessScript
+		}
+	}).witness;
+
+	tx.setWitness(0, witness);
+
+	console.log(await send(tx.toHex()));
+}
