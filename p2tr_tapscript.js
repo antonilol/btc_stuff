9dd456fbfa00ac0898ef1e69e6715cebb4e57cc3
Antoni Spaanderman <56turtle56@gmail.com>
2022-04-07 21:12:17 +0200

    add p2tr_tapscript.ts

--- p2tr_tapscript.js
+++ p2tr_tapscript.js
@@ -1,18 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 var curve = require("tiny-secp256k1");
 var bitcoin = require("bitcoinjs-lib");
 var btc_1 = require("./btc");
 var ecpair_1 = require("ecpair");
 var ECPair = (0, ecpair_1.ECPairFactory)(curve);
-var network = bitcoin.networks.testnet;
+var network = bitcoin.networks.regtest;
+(0, btc_1.setChain)('regtest');
 var hashtype = bitcoin.Transaction.SIGHASH_DEFAULT;
-// when you f*cked up a script taproot keyspend can save your coins!
-var keyspend = false;
-var ecpair1 = ECPair.fromPrivateKey(bitcoin.crypto.sha256(Buffer.from('<slam head on your keyboard for more randomness>addsadadasdads')));
-ecpair1.network = network;
-var ecpair2 = ECPair.fromPrivateKey(bitcoin.crypto.sha256(Buffer.from('<slam head on your keyboard for more randomness>asdasdasdasd')));
-ecpair2.network = network;
+var ecpair1 = ECPair.makeRandom({ network: network });
+while (ecpair1.publicKey[0] & 1) {
+    ecpair1 = ECPair.makeRandom({ network: network });
+}
+var ecpair2 = ECPair.makeRandom({ network: network });
 function tapLeaf(script) {
     return bitcoin.crypto.taggedHash('TapLeaf', Buffer.concat([Buffer.from([0xc0, script.length]), script]));
 }
@@ -22,7 +58,12 @@ function tapBranch(branch1, branch2) {
 function tapTweak(pubkey, branch) {
     return bitcoin.crypto.taggedHash('TapTweak', Buffer.concat([pubkey.slice(-32), branch]));
 }
-var script = bitcoin.script.compile([
+function createOutput(publicKey, tweak) {
+    var tweaked = curve.pointAddScalar(publicKey, tweak);
+    return { parity: tweaked[0] & 1, key: Buffer.from(tweaked).slice(-32) };
+}
+// build taptree
+var leaf1script = bitcoin.script.compile([
     ecpair2.publicKey.slice(1, 33),
     bitcoin.opcodes.OP_CHECKSIG,
     ecpair2.publicKey.slice(1, 33),
@@ -30,66 +71,58 @@ var script = bitcoin.script.compile([
     bitcoin.opcodes.OP_2,
     bitcoin.opcodes.OP_EQUAL
 ]);
-var leaf = tapLeaf(script);
-// Order of the curve (N) - 1
-var N_LESS_1 = Buffer.from('fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140', 'hex');
-// 1 represented as 32 bytes BE
-var ONE = Buffer.from('0000000000000000000000000000000000000000000000000000000000000001', 'hex');
-var tweak = tapTweak(ecpair1.publicKey, leaf);
-function createOutput(publicKey) {
-    // x-only pubkey (remove 1 byte y parity)
-    var myXOnlyPubkey = publicKey.slice(1, 33);
-    var tweakResult = curve.xOnlyPointAddTweak(myXOnlyPubkey, tweak);
-    if (tweakResult === null)
-        throw new Error('Invalid Tweak');
-    var tweaked = tweakResult.xOnlyPubkey;
-    // incomplete scriptPubkey
-    return Buffer.from(tweaked);
-}
-function sign(messageHash, key) {
-    var privateKey = key.publicKey[0] === 2
-        ? key.privateKey
-        : curve.privateAdd(curve.privateSub(N_LESS_1, key.privateKey), ONE);
-    var newPrivateKey = curve.privateAdd(privateKey, tweak);
-    if (newPrivateKey === null)
-        throw new Error('Invalid Tweak');
-    return Buffer.from(curve.signSchnorr(messageHash, newPrivateKey));
-}
-var tr = createOutput(ecpair1.publicKey);
-console.log("send 1000 sats to ".concat(bitcoin.address.toBech32(tr, 1, network.bech32)));
-var tx = new bitcoin.Transaction();
-var txid = '1234....'; // txid hex here
-var vout = 0;
-tx.version = 2;
-tx.addInput(Buffer.from(txid, 'hex').reverse(), vout);
-var fee_sat = 100;
+var leaf1 = tapLeaf(leaf1script);
+var leaf2 = Buffer.alloc(32); // all zeros
+var branch = tapBranch(leaf1, leaf2);
+var tweak = tapTweak(ecpair1.publicKey, branch);
+var tr = createOutput(ecpair1.publicKey, tweak);
+var fee_sat = 150;
 var input_sat = 1000;
-tx.addOutput((0, btc_1.bech32toScriptPubKey)('tb1qbech32addresshere'), input_sat - fee_sat);
-var sighash = tx.hashForWitnessV1(0, // which input
-[
-    bitcoin.script.compile([
-        bitcoin.opcodes.OP_1,
-        tr
-    ])
-], // All previous outputs of all inputs
-[input_sat], // All previous values of all inputs
-hashtype, // sighash flag, DEFAULT is schnorr-only (DEFAULT == ALL)
-keyspend ? undefined : leaf);
-if (keyspend) {
-    var signature = sign(sighash, ecpair1);
-    tx.setWitness(0, [signature]);
-}
-else {
-    var signature = Buffer.from(curve.signSchnorr(sighash, ecpair2.privateKey));
-    var pub = ecpair1.publicKey;
-    pub.writeUint8(0xc0 | (pub[0] & 1));
-    var ctrl = Buffer.concat([pub]);
-    tx.setWitness(0, [
-        signature,
-        signature,
-        script,
-        ctrl
-    ]);
-}
-console.log(tx.toHex());
-(0, btc_1.send)(tx.toHex()).then(console.log);
+var address = bitcoin.address.toBech32(tr.key, 1, network.bech32);
+console.log(address);
+(0, btc_1.fundAddress)(address, input_sat).then(function (outpoint) { return __awaiter(void 0, void 0, void 0, function () {
+    var tx, _a, _b, _c, sighash, signature, pub, ctrl, decoded;
+    return __generator(this, function (_d) {
+        switch (_d.label) {
+            case 0:
+                tx = new bitcoin.Transaction();
+                tx.version = 2;
+                tx.addInput(Buffer.from(outpoint.txid, 'hex').reverse(), outpoint.vout);
+                _b = (_a = tx).addOutput;
+                _c = btc_1.bech32toScriptPubKey;
+                return [4 /*yield*/, (0, btc_1.getnewaddress)()];
+            case 1:
+                _b.apply(_a, [_c.apply(void 0, [_d.sent()]), input_sat - fee_sat]);
+                sighash = tx.hashForWitnessV1(0, // which input
+                [
+                    bitcoin.script.compile([
+                        bitcoin.opcodes.OP_1,
+                        tr.key
+                    ])
+                ], // All previous outputs of all inputs
+                [input_sat], // All previous values of all inputs
+                hashtype, // sighash flag, DEFAULT is schnorr-only (DEFAULT == ALL)
+                leaf1);
+                signature = Buffer.from(curve.signSchnorr(sighash, ecpair2.privateKey));
+                pub = ecpair1.publicKey;
+                pub.writeUint8(0xc0 | tr.parity);
+                ctrl = Buffer.concat([pub, leaf2]);
+                tx.setWitness(0, [
+                    signature,
+                    signature,
+                    leaf1script,
+                    ctrl
+                ]);
+                return [4 /*yield*/, (0, btc_1.decodeRawTransaction)(tx.toHex())];
+            case 2:
+                decoded = _d.sent();
+                console.log(JSON.stringify(decoded, null, 2));
+                console.log(tx.toHex());
+                return [4 /*yield*/, (0, btc_1.send)(tx.toHex())];
+            case 3:
+                _d.sent();
+                console.log('sendrawtransaction successful');
+                return [2 /*return*/];
+        }
+    });
+}); });
--- p2tr_tapscript.ts
+++ p2tr_tapscript.ts
@@ -1,25 +1,19 @@
 import * as curve from 'tiny-secp256k1';
 import * as bitcoin from 'bitcoinjs-lib';
-import { send, bech32toScriptPubKey } from './btc';
-import { ECPairFactory, ECPairInterface } from 'ecpair'
+import { send, bech32toScriptPubKey, fundAddress, getnewaddress, decodeRawTransaction } from './btc';
+import { ECPairFactory } from 'ecpair'
 
 const ECPair = ECPairFactory(curve);
 
 const network = bitcoin.networks.testnet;
 const hashtype = bitcoin.Transaction.SIGHASH_DEFAULT;
 
-// when you f*cked up a script taproot keyspend can save your coins!
-const keyspend = false;
+var ecpair1 = ECPair.makeRandom({ network });
+while (ecpair1.publicKey[0] & 1) {
+	ecpair1 = ECPair.makeRandom({ network });
+}
 
-const ecpair1 = ECPair.fromPrivateKey(
-	bitcoin.crypto.sha256(Buffer.from('<slam head on your keyboard for more randomness>addsadadasdads'))
-);
-ecpair1.network = network;
-
-const ecpair2 = ECPair.fromPrivateKey(
-	bitcoin.crypto.sha256(Buffer.from('<slam head on your keyboard for more randomness>asdasdasdasd'))
-);
-ecpair2.network = network;
+const ecpair2 = ECPair.makeRandom({ network });
 
 function tapLeaf(script: Buffer) {
 	return bitcoin.crypto.taggedHash('TapLeaf', Buffer.concat([ Buffer.from([ 0xc0, script.length ]), script ]));
@@ -33,7 +27,13 @@ function tapTweak(pubkey: Buffer, branch: Buffer) {
 	return bitcoin.crypto.taggedHash('TapTweak', Buffer.concat([ pubkey.slice(-32), branch ]));
 }
 
-const script = bitcoin.script.compile([
+function createOutput(publicKey: Buffer, tweak: Buffer) {
+	const tweaked = curve.pointAddScalar(publicKey, tweak);
+	return { parity: tweaked[0] & 1, key: Buffer.from(tweaked).slice(-32) };
+}
+
+// build taptree
+const leaf1script = bitcoin.script.compile([
 	ecpair2.publicKey.slice(1,33),
 	bitcoin.opcodes.OP_CHECKSIG,
 	ecpair2.publicKey.slice(1,33),
@@ -42,86 +42,58 @@ const script = bitcoin.script.compile([
 	bitcoin.opcodes.OP_EQUAL
 ]);
 
-const leaf = tapLeaf(script);
+const leaf1 = tapLeaf(leaf1script);
 
-// Order of the curve (N) - 1
-const N_LESS_1 = Buffer.from(
-	'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140',
-	'hex'
-);
-// 1 represented as 32 bytes BE
-const ONE = Buffer.from(
-	'0000000000000000000000000000000000000000000000000000000000000001',
-	'hex'
-);
+const leaf2 = Buffer.alloc(32); // all zeros
 
-const tweak = tapTweak(ecpair1.publicKey, leaf);
+const branch = tapBranch(leaf1, leaf2);
 
-function createOutput(publicKey: Buffer) {
-	// x-only pubkey (remove 1 byte y parity)
-	const myXOnlyPubkey = publicKey.slice(1, 33);
-	const tweakResult = curve.xOnlyPointAddTweak(myXOnlyPubkey, tweak);
-	if (tweakResult === null) throw new Error('Invalid Tweak');
-	const { xOnlyPubkey: tweaked } = tweakResult;
-	// incomplete scriptPubkey
-	return Buffer.from(tweaked);
-}
+const tweak = tapTweak(ecpair1.publicKey, branch);
 
-function sign(messageHash: Buffer, key: ECPairInterface) {
-	const privateKey =
-		key.publicKey[0] === 2
-			? key.privateKey
-			: curve.privateAdd(curve.privateSub(N_LESS_1, key.privateKey), ONE);
-	const newPrivateKey = curve.privateAdd(privateKey, tweak);
-	if (newPrivateKey === null) throw new Error('Invalid Tweak');
-	return Buffer.from(curve.signSchnorr(messageHash, newPrivateKey));
-}
+const tr = createOutput(ecpair1.publicKey, tweak);
 
-const tr = createOutput(ecpair1.publicKey);
-
-console.log(`send 1000 sats to ${bitcoin.address.toBech32(tr, 1, network.bech32)}`);
-
-const tx = new bitcoin.Transaction();
-
-const txid = '1234....'; // txid hex here
-const vout = 0;
-
-tx.version = 2;
-tx.addInput(Buffer.from(txid, 'hex').reverse(), vout);
-
-const fee_sat = 100;
+const fee_sat = 150;
 const input_sat = 1000;
 
-tx.addOutput(bech32toScriptPubKey('tb1qbech32addresshere'), input_sat - fee_sat);
+const address = bitcoin.address.toBech32(tr.key, 1, network.bech32);
+console.log(address);
 
-const sighash = tx.hashForWitnessV1(
-	0, // which input
-	[
-		bitcoin.script.compile([
-			bitcoin.opcodes.OP_1,
-			tr
-		])
-	], // All previous outputs of all inputs
-	[ input_sat ], // All previous values of all inputs
-	hashtype, // sighash flag, DEFAULT is schnorr-only (DEFAULT == ALL)
-	keyspend ? undefined : leaf
-);
+fundAddress(address, input_sat).then(async outpoint => {
+	const tx = new bitcoin.Transaction();
 
-if (keyspend) {
-	const signature = sign(sighash, ecpair1);
-	tx.setWitness(0, [ signature ]);
-} else {
+	tx.version = 2;
+	tx.addInput(Buffer.from(outpoint.txid, 'hex').reverse(), outpoint.vout);
+
+	tx.addOutput(bech32toScriptPubKey(await getnewaddress()), input_sat - fee_sat);
+
+	const sighash = tx.hashForWitnessV1(
+		0, // which input
+		[
+			bitcoin.script.compile([
+				bitcoin.opcodes.OP_1,
+				tr.key
+			])
+		], // All previous outputs of all inputs
+		[ input_sat ], // All previous values of all inputs
+		hashtype, // sighash flag, DEFAULT is schnorr-only (DEFAULT == ALL)
+		leaf1
+	);
+
 	const signature = Buffer.from(curve.signSchnorr(sighash, ecpair2.privateKey));
 	const pub = ecpair1.publicKey;
-	pub.writeUint8(0xc0 | (pub[0] & 1));
-	const ctrl = Buffer.concat([ pub ]);
+	pub.writeUint8(0xc0 | tr.parity);
+	const ctrl = Buffer.concat([ pub, leaf2 ]);
 	tx.setWitness(0, [
 		signature,
 		signature,
-		script,
+		leaf1script,
 		ctrl
 	]);
-}
 
-console.log(tx.toHex());
-send(tx.toHex()).then(console.log);
+	const decoded = await decodeRawTransaction(tx.toHex());
+	console.log(JSON.stringify(decoded, null, 2));
+	console.log(tx.toHex());
+
+	await send(tx.toHex());
+	console.log('sendrawtransaction successful');
+});
